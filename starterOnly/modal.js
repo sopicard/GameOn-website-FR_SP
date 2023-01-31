6dca5356828cdef9ae517cebf26567aa9f6553d6
Sophie Picard <sogautrelet@gmail.com>
2023-01-31 14:54:08 +0100

    closing modal and first html corrections

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,21 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closingCross = document.querySelector(".closing-cross");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+closingCross.addEventListener("click", closeModal);
+
+function closeModal () {
+  modalbg.style.display = "none";
 }
 
 
