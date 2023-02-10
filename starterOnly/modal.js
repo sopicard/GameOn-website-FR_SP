6b5a5c7c5e1921bbd285c06592d38d47aa1790a0
Sophie Picard <sogautrelet@gmail.com>
2023-02-10 10:13:55 +0100

    new version of validation form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,9 +13,9 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closingCross = document.querySelector(".closing-cross");
 const form = document.querySelector(".modal-form");
-const inputTextControl = document.querySelector(".text-control");
 const inputsTextControl = document.querySelectorAll(".text-control");
-const dataError = document.querySelectorAll(".data-error-visible");
+const inputsCheckboxInput = document.querySelectorAll(".checkbox-input");
+const formIsOk = document.querySelector("#form-is-ok");
 const send = document.querySelector("#btn-submit");
 
 
@@ -34,72 +34,39 @@ function closeModal() {
 }
 
 //validation form
+// form.addEventListener("submit", validate);
+
+send.addEventListener("click", validate);
 function validate(event) {
-
-  let error; 
-
-  for (let i=0; i<inputsTextControl.length; i++) {
-   
+  let error = false;
+  
+  for (let i = 0; i < inputsTextControl.length; i++) {
+    if (!inputsTextControl[i].checkValidity()) {
+      formData[i].classList.add("data-error-visible");
+      error = true;   
+    } else {
+      formData[i].classList.remove("data-error-visible");
+    }
   } 
+
+  for (let j = 0; j < inputsCheckboxInput.length; j++) {
+    if(!inputsCheckboxInput[j].checked) {
+      inputsCheckboxInput[j].parentNode.classList.add("data-error-visible");
+      error = true;   
+    } else {
+      inputsCheckboxInput[j].parentNode.classList.remove("data-error-visible");
+    }
+  }
+  
   if (error) {
     event.preventDefault();
     return false;
-  } else {
-      alert("Merci ! Votre réservation a été reçue.");
+  }
+  // Si il n'y a pas d'erreur, afficher le message de validation  
+  if (!error) {
+    formIsOk.style.display = "block";
+    event.preventDefault();
   }
 }
 
 
- // if (!inputsTextControl[i].checkValidity()) {
-    //   error = formData.setAttribute("data-error-visible", true);
-    // }
-
-
-
-
-
-
-
-
-// const firstName = document.querySelector("#first");
-// const lastName = document.querySelector("#last");
-// const email = document.querySelector("#email");
-// const birthDate = document.querySelector("#birthdate");
-// const quantity = document.querySelector("#quantity");
-
-
-//btn-submit event
-// send.addEventListener("click", validationForm);  
-// function validationForm(event) {
-
-// //function to check validity of inputs and send the form
- 
-//   if (!firstName.value || firstName.value.length < 2) {  
-//     errorFirst.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom";
-//     errorFirst.style.color = "red";  
-//     return false;
-//   } else if (!lastName.value || lastName.value.length < 2) {    
-//       errorFirst.textContent = "";
-//       errorLast.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du nom";
-//       errorLast.style.color = "red";
-//       return false;
-//   } else if (!email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-//       //regex to have a valid email adress
-//       errorLast.textContent = "";    
-//       errorEmail.textContent = "Veuillez entrer une adresse mail valide";
-//       errorEmail.style.color = "red";
-//       return false;
-//   } else if (!birthDate.value) {
-//       errorEmail.textContent = "";     
-//       errorBirthdate.textContent = "Veuillez entrer votre date de naissance";
-//       errorBirthdate.style.color = "red";
-//       return false;
-//   } else if (!quantity.value) {
-//       errorBirthdate.textContent = "";     
-//       errorQuantity.textContent = "Veuillez saisir une valeur numérique";
-//       errorQuantity.style.color = "red";
-//       return false;
-//   } else {
-//       
-//   }
-// }
