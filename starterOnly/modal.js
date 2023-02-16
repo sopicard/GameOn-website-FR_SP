504f55ca7a4dd7143cbb7e5047c7b0cdcf24e136
Sophie Picard <sogautrelet@gmail.com>
2023-02-16 22:07:16 +0100

    modifications on validate function and create closeModal function

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,72 +1,76 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  var x = document.getElementById("myTopnav")
   if (x.className === "topnav") {
-    x.className += " responsive";
+    x.className += " responsive"
   } else {
-    x.className = "topnav";
+    x.className = "topnav"
   }
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const closingCross = document.querySelector(".closing-cross");
-const form = document.querySelector(".modal-form");
-const inputsTextControl = document.querySelectorAll(".text-control");
-const inputsCheckboxInput = document.querySelectorAll(".checkbox-input");
-const formIsOk = document.querySelector("#form-is-ok");
-const send = document.querySelector("#btn-submit");
-
+const modalbg = document.querySelector(".bground")
+const modalBtn = document.querySelectorAll(".modal-btn")
+const formData = document.querySelectorAll(".formData")
+const closeElements = document.querySelectorAll(".close")
+const form = document.querySelector(".modal-form")
+const inputsTextControl = document.querySelectorAll(".text-control")
+const radiosDiv = document.querySelector("#radios-div")
+const modalConfirmation = document.querySelector("#form-ok")
+const send = document.querySelector("#btn-submit")
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal))
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = "block"
 }
 
-//closing modal form
-closingCross.addEventListener("click", closeModal);
-function closeModal() {
-  modalbg.style.display = "none";
+//close each modals with btn or cross
+for (let i=0; i < closeElements.length; i++) {
+  closeElements[i].addEventListener("click", closeModal)
+}
+
+function closeModal(event) {
+  event.preventDefault()
+  const modal = event.target.closest(".modal")
+  modal.style.display = "none"
 }
 
 //validation form
-// form.addEventListener("submit", validate);
+// form.addEventListener("submit", validate)
 
-send.addEventListener("click", validate);
-function validate(event) {
-  let error = false;
-  
-  for (let i = 0; i < inputsTextControl.length; i++) {
-    if (!inputsTextControl[i].checkValidity()) {
-      formData[i].classList.add("data-error-visible");
-      error = true;   
-    } else {
-      formData[i].classList.remove("data-error-visible");
-    }
-  } 
+send.addEventListener("click", validate)
+function validate (event){
+  event.preventDefault()
+  let error = false
 
-  for (let j = 0; j < inputsCheckboxInput.length; j++) {
-    if(!inputsCheckboxInput[j].checked) {
-      inputsCheckboxInput[j].parentNode.classList.add("data-error-visible");
-      error = true;   
+  //checking each form's input
+  inputsTextControl.forEach ((input) => {
+    const formData = input.parentNode
+    if (!input.checkValidity()) {
+      formData.classList.add("data-error-visible")  
+      error = true 
     } else {
-      inputsCheckboxInput[j].parentNode.classList.remove("data-error-visible");
+      formData.classList.remove("data-error-visible")
     }
+  })
+
+  //checking radios buttons
+  const isChecked = radiosDiv.querySelector("input:checked")
+  if (!isChecked) {
+    radiosDiv.classList.add("data-error-visible")
+    error = true
+  } else {
+    radiosDiv.classList.remove("data-error-visible")
   }
-  
+
   if (error) {
-    event.preventDefault();
     return false;
-  }
-  // Si il n'y a pas d'erreur, afficher le message de validation  
-  if (!error) {
-    formIsOk.style.display = "block";
-    event.preventDefault();
+  } else if (!error) {
+    modalConfirmation.style.display = "block";
   }
 }
+
 
 
